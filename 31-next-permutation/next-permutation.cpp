1d4023687ec9d0a4cce365578251369f19a5e689
karanbisht10k <bishtkaran803@gmail.com>
2026-02-10 23:49:48 +0530

    Time: 0 ms (100.00%) | Memory: 15.8 MB (32.30%) - LeetSync

---
+++ 31-next-permutation/next-permutation.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int n = nums.size();
+        int i = n - 2;
+
+        // Step 1: find break point
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+
+        // Step 2: find next greater element
+        if (i >= 0) {
+            int j = n - 1;
+            while (nums[j] <= nums[i]) {
+                j--;
+            }
+            swap(nums[i], nums[j]);
+        }
+
+        // Step 3: reverse suffix
+        reverse(nums.begin() + i + 1, nums.end());
+    }
+};
+
