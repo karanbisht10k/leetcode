aee69405d9fcf976bb3b8f8bbf8e6fddb7b1f88c
KARAN BISHT <bishtkaran803@gmail.com>
2026-07-01 16:13:28 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (55.66%) - LeetSync

---
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int myAtoi(string s) {
+
+        int i = 0;
+        int n = s.length();
+
+        // Skip spaces
+        while (i < n && s[i] == ' ')
+            i++;
+
+        // Check sign
+        int sign = 1;
+        if (i < n && (s[i] == '+' || s[i] == '-')) {
+            if (s[i] == '-')
+                sign = -1;
+            i++;
+        }
+
+        long long ans = 0;
+
+        // Read digits
+        while (i < n && isdigit(s[i])) {
+
+            int digit = s[i] - '0';
+
+            // Overflow check
+            if (ans > (INT_MAX - digit) / 10) {
+                return sign == 1 ? INT_MAX : INT_MIN;
+            }
+
+            ans = ans * 10 + digit;
+            i++;
+        }
+
+        return ans * sign;
+    }
+};
