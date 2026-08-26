0dadf46453af11f38543780a1928c7ab181fb3ae
KARAN BISHT <bishtkaran803@gmail.com>
2026-08-27 00:52:51 +0530

    Time: 4 ms (43.27%) | Memory: 11 MB (72.24%) - LeetSync

---
+++ 71-simplify-path/simplify-path.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string simplifyPath(string path) {
+        vector<string> st;
+        string temp;
+
+        for (int i = 0; i <= path.size(); i++) {
+            if (i == path.size() || path[i] == '/') {
+                if (temp == "..") {
+                    if (!st.empty()) st.pop_back();
+                } else if (!temp.empty() && temp != ".") {
+                    st.push_back(temp);
+                }
+                temp = "";
+            } else {
+                temp += path[i];
+            }
+        }
+        string ans = "";
+        for (string s : st)
+            ans += "/" + s;
+
+        return ans.empty() ? "/" : ans;
+    }
+};
